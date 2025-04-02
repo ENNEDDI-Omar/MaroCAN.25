a44b86e6b4925f27e4ca4a8259ac0aa0d5a9e61d
ENNEDDI-Omar <enneddiomar@gmail.com>
2025-04-02 23:57:50 +0000

    feat: mise en place de l'entité et du changlog

---
+++ server/src/main/java/com/projects/server/domain/entities/Role.java
@@ -0,0 +1,31 @@
+package com.projects.server.domain.entities;
+
+import com.projects.server.domain.enums.RoleType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleType name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+}
---
+++ server/src/main/java/com/projects/server/domain/enums/RoleType.java
@@ -0,0 +1,9 @@
+package com.projects.server.domain.enums;
+
+public enum RoleType
+{
+    USER,
+    ADMIN,
+    VOLONTAIRE,
+    MEDIA
+}
